3c3731f1f61c77be338e055a17fb3ed1fa779911
Matteo <matteodurso45@gmail.com>
2024-03-19 12:50:42 +0100

    ese2

---
+++ RandomID.js
@@ -0,0 +1,7 @@
+const { 
+    v1: uuidv1,
+    v4: uuidv4,
+  } = require('uuid');
+  
+ console.log( uuidv1()) // -> '6c84fb90-12c4-11e1-840d-7b25c5ee775a' 
+  uuidv4(); // -> '110ec58a-a0f2-4ac4-8393-c866d813b8d1'
